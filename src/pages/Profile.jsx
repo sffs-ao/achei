b43ad762cee4234e6c85b5e0a8f4a578a132295f
Manuel Cardoso <manuelcardosomasc@gmail.com>
2024-09-07 11:24:59 +0100

    novas alterações

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,7 +9,7 @@ const userData = {
     "https://images.unsplash.com/photo-1595152769006-2b3b60e49122?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwyMDg0MjN8MHwxfGFsbHwxfHx8fHx8fHwxNjcwMjE0NzUy&ixlib=rb-1.2.1&q=80&w=400",
   position: "Desenvolvedora Front-end",
   bio: "Maria é uma desenvolvedora apaixonada por criar interfaces dinâmicas e eficientes usando React.",
-  phone: "+55 (11) 99999-9999",
+  phone: "+244 912 345 678",
 };
 
 export default function Profile() {
