3b06393ae8d35247329119172ba4a482ade4c484
Fernando Silva <fernandowonder123@gmail.com>
2024-09-28 19:32:26 +0100

    chatbox add feat

--- src/pages/MyCourseView/MyCourseView.tsx
+++ src/pages/MyCourseView/MyCourseView.tsx
@@ -1,8 +1,11 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import {useForm } from "react-hook-form"
+import { Loader2, Send } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const summary = [
     {
@@ -18,6 +21,9 @@ const summary = [
         "title": "Contagem de numeros",
     },
 ]
+export type FormData = {
+    message:string
+};
 
 async function POST_MESSAGE(data:string) {
     const response = await fetch('http://localhost:3000/message', { 
@@ -33,10 +39,20 @@ async function POST_MESSAGE(data:string) {
 export default function MyCourseView() {
  const { id } = useParams<{ id: string }>(); 
  const[update, setUpdaet] = useState(false)
- 
+ const[isLoading, setIsLoading] = useState(false)
+const form = useForm<FormData>()
 
   const[messageList,setMessagesList] = useState<{message:string, sender:string}[]>([])
+  const  divMessage = useRef<HTMLDivElement>(null)
 
+  useEffect(() => {
+    if (divMessage.current) {
+        divMessage.current.scrollTo({
+            top: divMessage.current.scrollHeight,
+            behavior: "smooth", // Animação suave
+          });
+    }
+  },[messageList])
     useEffect(() => {
        async function firstMessage() {
             const message = await POST_MESSAGE('O que é o verbo to be')
@@ -49,27 +65,40 @@ export default function MyCourseView() {
        }
        
     },[id])
+async function handleSubmit(data:FormData) {
+    setIsLoading(true)
+    setMessagesList((state) => [...state, {message:data.message, sender: "me"}])
+    await POST_MESSAGE(data.message)
+        .then((response) => {
+            setMessagesList((state) => [...state, {message:response.message, sender: response.sender}])
+    })
+    form.reset()
+    setIsLoading(false)
+}
 
-    console.log(messageList)
     return (
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 items-start">
-            <div className="border shadow-sm rounded-sm md:flex-1 p-4 flex flex-col gap-4 min-h-[calc(100vh-150px)] overflow-y-auto ">
-                    <div className="  h-[calc(100vh-200px)] overflow-y-auto">
+            <div className="w-full border shadow-sm rounded-sm md:flex-1 p-4 flex flex-col gap-4 min-h-[calc(100vh-150px)] overflow-y-auto ">
+                    <div className="  h-[calc(100vh-200px)] overflow-y-auto" ref={divMessage}>
                         <h1 className="text-lg font-bold">O que é o verbo to be</h1>
-                        <div className=" ">
-
+                        <div className="flex flex-col gap-4" >
+                            
                             {
                                 messageList.map((message, index)=> (
-                                    <p key={index} className={`${message.sender === "me" ? "bg-green-900 text-zinc-100 " : "bg-zinc-100 text-zinc-800"} p-4 text-sm leading-loose rounded-sm`}>
-                                        {message.message.replace("*", "").replace("**", "")}
-                                    </p>
+                                    <div key={index} className={`${message.sender === "me" ? "bg-green-900 text-zinc-100 " : "bg-zinc-100 text-zinc-800"} p-2 text-sm leading-loose flex flex-col w-fit rounded-md max-w-[800px]  `}>
+                                           {message.message.split("\n").map((line, index) => (
+                                            <p key={index}>{line}</p>
+                                        ))}
+                                    </div>
                                 ))
                             }
+                            {isLoading && <span className="text-sm font-bold text-zinc-700 flex gap-2 items-center">Espere um pouco <Loader2 className="mr-2 h-4 w-4 animate-spin" /></span>}
                          </div>
                     </div>
                     <div className="h-10 ">
-                        <form>
-                            <Input className="focus-visible:ring-0 " type="text" placeholder="Pergunte algo" />
+                        <form onSubmit={form.handleSubmit(handleSubmit)} className="flex">
+                            <Input required disabled={isLoading} {...form.register("message")} className="focus-visible:ring-0 rounded-r-none" type="text" placeholder="Pergunte algo" />
+                            <Button disabled={isLoading} className="rounded-l-none"><Send/></Button>
                         </form>
                     </div>            
             </div>
