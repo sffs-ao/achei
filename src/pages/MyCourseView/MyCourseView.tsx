f0a7a9e9750817712c6017f1b339f2ee424c0255
Fernando Silva <fernandowonder123@gmail.com>
2024-10-03 17:47:46 +0100

    change url bot

--- src/pages/MyCourseView/MyCourseView.tsx
+++ src/pages/MyCourseView/MyCourseView.tsx
@@ -26,7 +26,7 @@ export type FormData = {
 };
 
 async function POST_MESSAGE(data:string) {
-    const response = await fetch('http://localhost:3000/message', { 
+    const response = await fetch('https://faceapi-api2.onrender.com', { 
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
