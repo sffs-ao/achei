f9789b5d11e6c46fe7b1b62bfdb6263d39421bd1
Manuel Cardoso <manuelcardosomasc@gmail.com>
2024-10-08 18:29:56 +0100

    add user profile

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,51 +3,52 @@ import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div>
-            <h1 className="mb-8">Olá, Fernando! </h1>
-            <div className="grid  items-start gap-4">
-                <Card>
-                    <CardHeader>
-                        <span>Meu perfil</span>
-                    </CardHeader>
-                    <CardContent>
-                    <Link to="/me"> <Button>Ver perfil</Button></Link>
-                        <div className="mt-4">
-                            <span className="font-semibold text-zinc-900 text-md">Nivel básico</span>
-                            <div className="bg-zinc-300 w-full h-6 rounded-lg  relative">
-                                <div className="h-6 bg-green-500 rounded-lg w-1/4 ">
-                                    <span className="text-xs font-semibold absolute right-2/4 top-2/4 translate-x-2/4 -translate-y-2/4 ">25%</span>
-                                </div>
-                            </div>
-                        </div>
-                    
-                    </CardContent>
-                </Card>
-
-                <div className="flex flex-col max-md:mt-8 md:max-w-[512px]">
-                    <h1 className="mb-2">Agenda</h1>
-                    <div className="flex flex-col gap-2 max-h-[340px] overflow-y-auto">
-
-                     <Link to="">
-                        <div className="h-20 flex rounded-sm overflow-hidden">
-                            <div className="bg-primary flex flex-col items-center p-4 text-white" >
-                                    <span className="text-2xl font-semibold">26</span>
-                                    <span className="text-sm ">setembro</span>
-                                </div> 
-                                <div className="flex pl-4 flex-col border shadow-sm w-full justify-center">
-                                    <span className="font-semibold">Exame final</span>
-                                    <span className="text-sm text-zinc-700">11:00 AM</span>
-                                </div>
-                            </div>
-                     </Link>
-                    
-
-               
-
-                    </div>
+  return (
+    <div>
+      <h1 className="mb-8">Olá, Fernando! </h1>
+      <div className="grid items-start gap-4">
+        <Card>
+          <CardHeader>
+            <span>Meu perfil</span>
+          </CardHeader>
+          <CardContent>
+            <Link to="/portal/me">
+              {" "}
+              <Button>Ver perfil</Button>
+            </Link>
+            <div className="mt-4">
+              <span className="font-semibold text-zinc-900 text-md">
+                Nivel básico
+              </span>
+              <div className="relative w-full h-6 rounded-lg bg-zinc-300">
+                <div className="w-1/4 h-6 bg-green-500 rounded-lg ">
+                  <span className="absolute text-xs font-semibold right-2/4 top-2/4 translate-x-2/4 -translate-y-2/4 ">
+                    25%
+                  </span>
                 </div>
+              </div>
             </div>
+          </CardContent>
+        </Card>
+
+        <div className="flex flex-col max-md:mt-8 md:max-w-[512px]">
+          <h1 className="mb-2">Agenda</h1>
+          <div className="flex flex-col gap-2 max-h-[340px] overflow-y-auto">
+            <Link to="">
+              <div className="flex h-20 overflow-hidden rounded-sm">
+                <div className="flex flex-col items-center p-4 text-white bg-primary">
+                  <span className="text-2xl font-semibold">26</span>
+                  <span className="text-sm ">setembro</span>
+                </div>
+                <div className="flex flex-col justify-center w-full pl-4 border shadow-sm">
+                  <span className="font-semibold">Exame final</span>
+                  <span className="text-sm text-zinc-700">11:00 AM</span>
+                </div>
+              </div>
+            </Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -4,134 +4,168 @@ import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import DatePicker from "./DatePickerBirth";
 import { useState } from "react";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { GraduationCap } from "lucide-react";
+import userProfile from "../../assets/user-profile.png";
 
 export default function ProfilePage() {
-    return (
-        <div className="flex flex-col gap-6 md:flex-row items-start">
-            <Card className="w-full md:w-fit">
-                <CardHeader className="relative h-20">
-                    <div className="absolute bg-primary h-28 w-28 rounded-full 
-                    flex items-center justify-center -bottom-28 left-2/4 -translate-y-2/4 -translate-x-2/4 
-                    ">
-                        <CardTitle className="text-white">FS</CardTitle>
-                    </div>
-                </CardHeader>
+  return (
+    <div className="flex flex-col items-start gap-6 md:flex-row">
+      <Card className="w-full md:w-fit">
+        <CardHeader className="relative h-20">
+          <div className="absolute flex items-center justify-center rounded-full h-28 w-28 -bottom-28 left-2/4 -translate-y-2/4 -translate-x-2/4 ">
+            <img src={userProfile} alt="" />
+            {/* <CardTitle className="text-white">FS</CardTitle> */}
+          </div>
+        </CardHeader>
 
-
-                <CardContent className="pt-16 bg-zinc-100 flex justify-center items-center flex-col">
-                    <h1 className="text-lg font-bold text-center">Fernando Silva</h1>
-                    <span className="text-center font-normal text-zinc-600">fernandowonder123@gmail.com</span>
-                </CardContent>
-
-            </Card>
-            <div className="w-full">
-            <Tabs defaultValue="account" className="w-full">
-                <TabsList>
-                    <TabsTrigger value="account">Minha conta</TabsTrigger>
-                    <TabsTrigger value="access">Acesso</TabsTrigger>
-                    <TabsTrigger value="grade">Certificados</TabsTrigger>
-                </TabsList>
-                <TabsContent className="w-full shadow-sm border bg-white p-4 rounded-md" value="account"><MyAccount/></TabsContent>
-                <TabsContent className="w-full shadow-sm border bg-white p-4 rounded-md" value="access"><Access/></TabsContent>
-                <TabsContent className="w-full shadow-sm border bg-white p-4 rounded-md" value="grade"><Grade/></TabsContent>
-                </Tabs>
-            </div>
-        </div>
-    )
+        <CardContent className="flex flex-col items-center justify-center pt-16 bg-zinc-100">
+          <h1 className="text-lg font-bold text-center">Fernando Silva</h1>
+          <span className="font-normal text-center text-zinc-600">
+            fernandowonder123@gmail.com
+          </span>
+        </CardContent>
+      </Card>
+      <div className="w-full">
+        <Tabs defaultValue="account" className="w-full">
+          <TabsList>
+            <TabsTrigger value="account">Minha conta</TabsTrigger>
+            <TabsTrigger value="access">Acesso</TabsTrigger>
+            <TabsTrigger value="grade">Certificados</TabsTrigger>
+          </TabsList>
+          <TabsContent
+            className="w-full p-4 bg-white border rounded-md shadow-sm"
+            value="account"
+          >
+            <MyAccount />
+          </TabsContent>
+          <TabsContent
+            className="w-full p-4 bg-white border rounded-md shadow-sm"
+            value="access"
+          >
+            <Access />
+          </TabsContent>
+          <TabsContent
+            className="w-full p-4 bg-white border rounded-md shadow-sm"
+            value="grade"
+          >
+            <Grade />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export function MyAccount() {
-    const [date, setDate] = useState<Date>()
-    return (
-    
-            <form className="flex flex-col gap-4 w-full">
-                <h1>Dados pessoais</h1>
-               <div className="flex flex-col md:flex-row md:flex-1 gap-4 w-full items-center">
-                    <fieldset className="w-full flex flex-col gap-2 md:flex-1 min-w-80">
-                        <Label>Nome</Label>
-                        <Input placeholder="Seu nome" type="text"/>
-                    </fieldset>
-                    <fieldset className="flex flex-col gap-2 w-full md:w-72">
-                        <Label>Telefone</Label>
-                        <Input placeholder="number" type="seu Telefone"/>
-                    </fieldset>
-               </div>
-                <fieldset className="w-full flex flex-col gap-2">
-                    <Label>Data de nascimento</Label>
-                    <DatePicker  selectedDate={date} setSelectedDate={setDate} />
-                </fieldset>
-                <fieldset className="w-full flex flex-col gap-2">
-                    <Label>Email</Label>
-                    <Input placeholder="" value={"fernandowonder123@gmail.com"} disabled type="email"/>
-                </fieldset>
-                <div className="flex flex-col md:flex-row md:flex-1 gap-4 w-full items-center">
-                    <fieldset className="w-full flex flex-col gap-2 md:flex-1">
-                        <Label>Selecione tipo de documento</Label>
-                        <Select>
-                            <SelectTrigger className="w-full">
-                                <SelectValue placeholder="Tipo de documento" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectGroup>
-                                <SelectLabel>Fruits</SelectLabel>
-                                <SelectItem value="apple">Bilhere de ID</SelectItem>
-                                <SelectItem value="banana">Passaporte</SelectItem>
-                                <SelectItem value="blueberry">Certidao</SelectItem>
-                                </SelectGroup>
-                            </SelectContent>
-                        </Select>
-                    </fieldset>
-                    <fieldset className="flex flex-col gap-2 w-full md:flex-1">
-                        <Label>Nº de identificação</Label>
-                        <Input placeholder="number" type="seu Telefone"/>
-                    </fieldset>
-               </div>
-               <div className="flex items-end justify-end">
-                    <Button>Salvar</Button>
-               </div>
-            </form>
-    
-    )
+  const [date, setDate] = useState<Date>();
+  return (
+    <form className="flex flex-col w-full gap-4">
+      <h1>Dados pessoais</h1>
+      <div className="flex flex-col items-center w-full gap-4 md:flex-row md:flex-1">
+        <fieldset className="flex flex-col w-full gap-2 md:flex-1 min-w-80">
+          <Label>Nome</Label>
+          <Input placeholder="Seu nome" type="text" />
+        </fieldset>
+        <fieldset className="flex flex-col w-full gap-2 md:w-72">
+          <Label>Telefone</Label>
+          <Input placeholder="number" type="seu Telefone" />
+        </fieldset>
+      </div>
+      <fieldset className="flex flex-col w-full gap-2">
+        <Label>Data de nascimento</Label>
+        <DatePicker selectedDate={date} setSelectedDate={setDate} />
+      </fieldset>
+      <fieldset className="flex flex-col w-full gap-2">
+        <Label>Email</Label>
+        <Input
+          placeholder=""
+          value={"fernandowonder123@gmail.com"}
+          disabled
+          type="email"
+        />
+      </fieldset>
+      <div className="flex flex-col items-center w-full gap-4 md:flex-row md:flex-1">
+        <fieldset className="flex flex-col w-full gap-2 md:flex-1">
+          <Label>Selecione tipo de documento</Label>
+          <Select>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Tipo de documento" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                <SelectItem value="apple">Bilhere de ID</SelectItem>
+                <SelectItem value="banana">Passaporte</SelectItem>
+                <SelectItem value="blueberry">Certidao</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </fieldset>
+        <fieldset className="flex flex-col w-full gap-2 md:flex-1">
+          <Label>Nº de identificação</Label>
+          <Input placeholder="number" type="seu Telefone" />
+        </fieldset>
+      </div>
+      <div className="flex items-end justify-end">
+        <Button>Salvar</Button>
+      </div>
+    </form>
+  );
 }
 
 export function Access() {
-    return (
-        <div className="flex flex-col gap-4 w-full">
-             <h1>Informações de acesso</h1>
-            <fieldset>
-                <Label>Email</Label>
-                <Input disabled value={"fernandowonder123@gmail.com"}/>
-            </fieldset>
-            <fieldset>
-                <Label>Senha</Label>
-                <div className="flex items-center">
-                    <Input className="rounded-r-none" value={"************"} disabled placeholder=""/>
-                    <Button className="rounded-l-none bg-red-800 hover:bg-red-900">Mudar senha</Button>
-                </div>
-            </fieldset>
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <h1>Informações de acesso</h1>
+      <fieldset>
+        <Label>Email</Label>
+        <Input disabled value={"fernandowonder123@gmail.com"} />
+      </fieldset>
+      <fieldset>
+        <Label>Senha</Label>
+        <div className="flex items-center">
+          <Input
+            className="rounded-r-none"
+            value={"************"}
+            disabled
+            placeholder=""
+          />
+          <Button className="bg-red-800 rounded-l-none hover:bg-red-900">
+            Mudar senha
+          </Button>
         </div>
-    )
+      </fieldset>
+    </div>
+  );
 }
 
-
 export function Grade() {
-    return (
-        <div>
-            <h1>Certificados</h1>
-            <div className="flex flex-col mt-2">
-                <div className="border shadow-sm flex h-14 items-center rounded-sm overflow-hidden">
-                    <div className="bg-primary text-white h-14 flex items-center w-14 justify-center">
-                        <GraduationCap/>
-                    </div>
-                    <div className="flex items-center justify-between w-full">
-                        <span className="ml-2">Curso de ingles</span>
-                        <Button className="" variant={"link"}>Obter certificado</Button>
-                    </div>
-                </div> 
-            </div>
+  return (
+    <div>
+      <h1>Certificados</h1>
+      <div className="flex flex-col mt-2">
+        <div className="flex items-center overflow-hidden border rounded-sm shadow-sm h-14">
+          <div className="flex items-center justify-center text-white bg-primary h-14 w-14">
+            <GraduationCap />
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <span className="ml-2">Curso de ingles</span>
+            <Button className="" variant={"link"}>
+              Obter certificado
+            </Button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
